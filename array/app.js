5c3849c36cce09b8883ecc5bb217603e32b6de22
nassim <bladess@hotmail.fr>
2017-07-05 11:19:11 +0200

    les mois c'est fait

--- array/app.js
+++ array/app.js
@@ -1,1 +1,9 @@
 var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
+var mois = ['Janvier','Fevrier','Mars','Avril','Mai','Juin','Juillet','Aout','Septembre','Octobre','Novembre','Decembre'];
+console.log(mois[2]);
+console.log(mois[5]);
+console.log(mois[10]);
+mois[8] = 'Août';
+for(var i=0;i < 12;i++){
+	console.log(mois[i]);
+}
